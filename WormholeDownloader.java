347c6c376214eadf20e0ffd0a38474348567c811
Matthew Bichay <mattbichay@gmail.com>
2015-12-10 20:21:54 -0500

    Wormhole download implemented.

--- WormholeDownloader.java
+++ WormholeDownloader.java
@@ -4,6 +4,15 @@ import java.net.URLConnection;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+
+
 
 public class WormholeDownloader{
 
@@ -11,6 +20,8 @@ public class WormholeDownloader{
 	private String urlStr;
 	private HttpURLConnection httpConn;
 	private URL url;
+	OutputStream oStream;
+    PrintWriter writer;
 
 
 
@@ -54,6 +65,49 @@ public class WormholeDownloader{
 	}
 
 	public Boolean download(String filename){
+		String boundary = filename+ "===" + System.currentTimeMillis() + "===";
+
+		try {
+			url = new URL(urlStr);
+			httpConn = (HttpURLConnection)url.openConnection();
+			httpConn.setDoOutput(true);
+			httpConn.setDoInput(true);
+			httpConn.setRequestProperty("Content-Type",
+	                "multipart/form-data; boundary=" + boundary);
+
+			oStream = httpConn.getOutputStream();
+			writer = new PrintWriter(new OutputStreamWriter(oStream, Wormhole.CHAR_SET), true);
+			writer.flush();
+			writer.close();
+			
+
+			if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK){
+				//BufferedReader reader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+				//List items = 
+				//String response;
+
+				BufferedInputStream bis = new BufferedInputStream(httpConn.getInputStream());
+				File test = new File(filename);
+				BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream(test.getName()));
+
+				int i = -1;
+				byte[] arry = new byte[4096];
+				while ((i = bis.read(arry)) > 0) {
+					System.out.println(i);
+					bos.write(arry, 0, i);
+				}
+				bos.flush();
+				bos.close();
+			}
+
+
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+
+
 		System.out.println("Feature not yet implemented.");
 		return false;
 	}
