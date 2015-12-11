68d41631cf2d639a336b4c12fa8f7bc88d2268be
Matthew Bichay <mattbichay@gmail.com>
2015-12-10 20:27:53 -0500

    dl functionality

--- WormholeServletApp/src/wormhole/WormholeDownload.java
+++ WormholeServletApp/src/wormhole/WormholeDownload.java
@@ -1,7 +1,9 @@
 package wormhole;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -17,6 +19,28 @@ public class WormholeDownload extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println(getUsername(request.getContentType()));
+		
+		String filepath = getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator + "Matt2" + File.separator + getUsername(request.getContentType());
+		File file = new File(filepath);
+		
+		FileInputStream iStream = new FileInputStream(file);
+		byte[] buffer = new byte[4096];
+		int bytesRead = -1;
+		long totalBytes = 0;
+		long fileSize = file.length();
+		
+		OutputStream oStream = response.getOutputStream();
+		
+		while ((bytesRead = iStream.read(buffer)) != -1){
+			oStream.write(buffer, 0, bytesRead);
+			totalBytes += bytesRead;
+			System.out.println("Bytes Uploaded: " + totalBytes);
+		}
+		iStream.close();
+		oStream.flush();
+		oStream.close();
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -45,4 +69,11 @@ public class WormholeDownload extends HttpServlet {
 			System.out.println(e);
 		}
 	}
+	
+	
+	private String getUsername(String boundary){
+		String username = boundary.substring(boundary.indexOf("boundary=") + "boundary=".length());
+		return username.substring(0, username.indexOf('='));
+	}
 }
+
