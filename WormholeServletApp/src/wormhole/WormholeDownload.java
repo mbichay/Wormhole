a552b9f81c270d9bbb0c3f0b22f0306c6ef3e88c
Matthew Bichay <mattbichay@gmail.com>
2015-12-12 17:19:58 -0500

    re-based servlet app

--- WormholeServletApp/src/wormhole/WormholeDownload.java
+++ WormholeServletApp/src/wormhole/WormholeDownload.java
@@ -5,75 +5,76 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class WormholeDownload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public WormholeDownload() {
-        super();
-    }
-
+      
+	//File download
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Check if multipart request. If not, fail out.
+		if (!ServletFileUpload.isMultipartContent(request)){
+			return;
+		}
 		
-		System.out.println(getUsername(request.getContentType()));
-		
-		String filepath = getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator + "Matt2" + File.separator + getUsername(request.getContentType());
+		//Attain filepath from URL username and filename.
+		String filepath = getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator +
+						WormholeUtils.parseOption(request.getContentType(), "username=") + File.separator + 
+						WormholeUtils.parseOption(request.getContentType(), "filename=");
 		File file = new File(filepath);
 		
+		//Open up file stream from the filepath.
 		FileInputStream iStream = new FileInputStream(file);
 		byte[] buffer = new byte[4096];
 		int bytesRead = -1;
-		long totalBytes = 0;
-		long fileSize = file.length();
 		
+		//Open up response output stream and write to the user in bytes.
 		OutputStream oStream = response.getOutputStream();
-		
 		while ((bytesRead = iStream.read(buffer)) != -1){
 			oStream.write(buffer, 0, bytesRead);
-			totalBytes += bytesRead;
-			System.out.println("Bytes Uploaded: " + totalBytes);
 		}
 		iStream.close();
 		oStream.flush();
 		oStream.close();
 	}
 	
+	//Files available in user account query.
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Check if valid username in URL
 		String username = request.getParameter("username");
 		if (username == null){
-			System.out.println("Please input username.");
 			return;
 		}
 		
+		//If the folder doesn't exist, make it.
+		//Design decision to avoid having a setup-process in the user-registration step.
 		File folder = new File(getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator + username);
 		if (!folder.exists()){
 			folder.mkdirs();
 		}
 		
+		//Pull the file name from the folder
 		File[] files = folder.listFiles();
 		
+		//Get the response printer and print files to user deliniated by the "//" option seperator.
 		PrintWriter printWriter = response.getWriter();
-		
 		try {
 			for (int i = 0; i < files.length; ++i){
 				if (files[i].isFile()){
-					printWriter.print(i+". " + files[i].getName() + "//");
+					printWriter.print(files[i].getName() + "//");
 				}
 			}
+			printWriter.flush();
+			printWriter.close();
+			
 		} catch (Exception e) {
-			System.out.println(e);
+			System.out.println(e); //Unexpected error.
 		}
-	}
-	
-	
-	private String getUsername(String boundary){
-		String username = boundary.substring(boundary.indexOf("boundary=") + "boundary=".length());
-		return username.substring(0, username.indexOf('='));
 	}
 }
 
--- WormholeServletApp/src/wormhole/WormholeUpload.java
+++ WormholeServletApp/src/wormhole/WormholeUpload.java
@@ -2,33 +2,52 @@ package wormhole;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.util.Iterator;
 import java.io.File;
 import java.util.List;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class WormholeUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String STORAGE_DIR = "wormhole";
+	//Temporary directory for storing intermediary buffer.
 	private static final String THRESHHOLD_TEMP_DIR = System.getProperty("java.io.tmpdir");
 
-
-    public WormholeUpload() {
-        super();
+    //File deletion
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	//Validate input parameters required for this function. Pulled from URL.
+    	String username = request.getParameter("username");
+    	String filename = request.getParameter("filename");
+    	if (username == null || filename == null){
+    		return;
+    	}
+    	
+    	//Get the writer for writing to the response.
+    	PrintWriter writer = response.getWriter();
+    	
+    	//Get the file specified by parameters.
+    	File file = new File(getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator +
+    				username + File.separator + filename);
+    	
+    	// File delete.
+    	if (file.delete()){
+    		writer.write("File deleted successfully.");
+    	} else {
+    		writer.write("File deletion failed.");
+    	}
+    	writer.flush();
+    	writer.close();	
     }
-
+    
+    
+    // File upload.
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//Check to see if the server post request to this servlet is a multipart upload
@@ -36,46 +55,49 @@ public class WormholeUpload extends HttpServlet {
 			return;
 		}
 		
-
-		DiskFileItemFactory fileFactory = new DiskFileItemFactory(DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD, new File(THRESHHOLD_TEMP_DIR));
+		//Create file factory and servletFileuploader.
+		DiskFileItemFactory fileFactory = new DiskFileItemFactory(DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD,
+										new File(THRESHHOLD_TEMP_DIR));
 		ServletFileUpload fileUpload = new ServletFileUpload(fileFactory);
-		PrintWriter printWriter = response.getWriter();
-		String servletPath = getServletContext().getRealPath("") + File.separator + STORAGE_DIR + File.separator + getUsername(request.getContentType());
 		
+		// Get response writer for responding to request.
+		PrintWriter printWriter = response.getWriter();
+		
+		// Get the path to the storage directory.
+		String servletPath = getServletContext().getRealPath("") + File.separator + "wormhole" + File.separator +
+				WormholeUtils.parseOption(request.getContentType(), "username=");
+		
+		//Create directory if doesn't exist.
 		File baseStorageDir = new File(servletPath);
 		if (!baseStorageDir.exists()){
 			baseStorageDir.mkdirs();
 		}
 		
 		try {
+			//Begin request parse.
 			List items = fileUpload.parseRequest(request);
 			Iterator itemIterator = items.iterator();
-		
+			
+			//Consume bytes.
 			while(itemIterator.hasNext()){
 				FileItem uploadItem = (FileItem) itemIterator.next();
 				if (uploadItem.isFormField()){
 					continue;
 				}
-				
+				//Write bytes out to disk.
 				String fileName = new File(uploadItem.getName()).getName();
                 String filePath = servletPath + File.separator + fileName;
                 File storeFile = new File(filePath);
-                System.out.println(filePath);
                 uploadItem.write(storeFile);
-                printWriter.print("File upload sucessful...\n");
-                printWriter.print("Name: " + uploadItem.getName() + "\n");
-                printWriter.print("Size: " + uploadItem.getSize() + " Bytes\n");
-                printWriter.print("Date: " + new SimpleDateFormat("dd-MM-YYYY").format(new Date()) + "\n");
+                System.out.println("File Uploaded: " + filePath);
+                printWriter.flush();
+                printWriter.close();
 			}
+			//Send successful response to user.
+			printWriter.print("File upload successful.");
 		} catch (Exception e) {
+			//Send failure response.
 			printWriter.print("File upload failure.");
 		}	
 	}
-	
-	private String getUsername(String boundary){
-		String username = boundary.substring(boundary.indexOf("boundary=") + "boundary=".length());
-		return username.substring(0, username.indexOf('='));
-	}
-	
-
 }
